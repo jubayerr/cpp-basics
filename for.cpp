343515f1fd9092145d6ca56b7f495d1011b8d1cb
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-08 09:36:05 +0600

    for loop

---
+++ for.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int randomNumbers[] = {2, 4, 5, 6, 8, 10};
+
+    for (int i = 0; i < 6; i++)
+    {
+        cout << randomNumbers[i] << endl;
+    }
+
+    return 0;
+}
