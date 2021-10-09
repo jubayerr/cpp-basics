ff4d5e730e7daed1728d3756eca213754bf40262
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-09 12:06:56 +0600

    objects

--- objects.cpp
+++ objects.cpp
@@ -5,15 +5,19 @@ using namespace std;
 
 class Book
 {
+private:
+    string rating;
+
 public:
     string title;
     string author;
     int pages;
-    Book(string aTitle, string aAuthor, int aPages)
+    Book(string aTitle, string aAuthor, int aPages, string aRating)
     {
         title = aTitle;
         author = aAuthor;
         pages = aPages;
+        setRating(aRating);
     }
     bool isCostly()
     {
@@ -26,18 +30,35 @@ public:
             return false;
         }
     }
+    void setRating(string aRating)
+    {
+        if (aRating == "G" || aRating == "PG" || aRating == "PG-13" || aRating == "R" || aRating == "NR")
+        {
+            rating = aRating;
+        }
+        else
+        {
+            rating = "NR";
+        }
+    }
+    string getRating()
+    {
+        return rating;
+    }
 };
 
 int main()
 {
-    Book book1("Harry Potter", "JK Rowling", 500);
+    Book book1("Harry Potter", "JK Rowling", 500, "PG-13");
 
-    Book book2("Lords of the Rings", "Tolkien", 700);
+    Book book2("Lords of the Rings", "Tolkien", 700, "G");
 
     cout << book1.title << endl;
     cout << book1.author << endl;
     cout << book1.pages << endl;
     cout << book2.isCostly() << endl;
+
+    cout << book1.getRating();
 
     return 0;
 }
