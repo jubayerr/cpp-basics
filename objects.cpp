d71413e304910411089492885bae1158d747c05b
Showkat Jamil Jubayer <showkatjubayer@gmail.com>
2021-10-09 11:26:19 +0600

    objects

--- objects.cpp
+++ objects.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 using namespace std;
 
+// Object
+
 class Book
 {
 public:
     string title;
     string author;
     int pages;
+    Book(string aTitle, string aAuthor, int aPages)
+    {
+        title = aTitle;
+        author = aAuthor;
+        pages = aPages;
+    }
 };
 
 int main()
 {
-    Book book1;
+    Book book1("Harry Potter", "JK Rowling", 500);
 
-    book1.title = "Harry Potter";
-    book1.author = "JK Rowling";
-    book1.pages = 500;
+    Book book2("Lords of the Rings", "Tolkien", 700);
 
-    Book book2;
-
-    book2.title = "Lord of the Rings";
-    book2.author = "Tolkein";
-    book2.pages = 700;
-
-    cout << book1.title;
+    cout << book1.title << endl;
+    cout << book1.author << endl;
+    cout << book1.pages << endl;
 
     return 0;
 }
